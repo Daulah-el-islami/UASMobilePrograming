00209b0b63c8cb90a19190d9b3a877654979eaa6
Daulah-el-islami <dhensof@gmail.com>
2024-07-18 18:41:35 +0700

    Updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,6 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.android.gms:play-services-auth")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
 }
